c489d2b8a7505b4f78931e6345c03075fc459bb9
srk-apts <37604450+srk-apts@users.noreply.github.com>
2019-06-10 17:26:45 -0500

    Add files via upload

--- utility_functions.py
+++ utility_functions.py
@@ -30,18 +30,24 @@ from sklearn.preprocessing import Imputer, StandardScaler, PolynomialFeatures
 # In[2]:
 
 
+def scale_numeric(fit_df, trans_df, cols):
+    df_scaled = trans_df[cols].copy()
+    scaler = StandardScaler().fit(fit_df[cols])
+    df_scaled = pd.DataFrame(scaler.transform(df_scaled), index=df_scaled.index, columns=cols)
+    return(df_scaled)
+
 def scale_encode(fit_df, trans_df):
     num_cols = list(fit_df.columns[(fit_df.dtypes == 'float64') | (fit_df.dtypes == 'int64')])
     str_cols = list(set(fit_df.columns)^set(num_cols))
     
-    df_scaled = trans_df[num_cols].copy()
-    scaler = StandardScaler().fit(fit_df[num_cols])
-    df_scaled = pd.DataFrame(scaler.transform(df_scaled), index=df_scaled.index, columns=num_cols)
-    
-    df_dummy = pd.get_dummies(trans_df[str_cols])
-    
-    df =  df_scaled.join(df_dummy)
-    
+    if (len(num_cols)>0) & (len(str_cols)>0):
+        df_scaled = scale_numeric(fit_df=fit_df, trans_df=trans_df, cols=num_cols)
+        df_dummy = pd.get_dummies(trans_df[str_cols])
+        df =  df_scaled.join(df_dummy)
+    elif len(num_cols)==0:
+        df = pd.get_dummies(trans_df[str_cols])
+    elif len(str_cols)==0:
+        df = scale_numeric(fit_df=fit_df, trans_df=trans_df, cols=num_cols)        
     return(df)
 
 
@@ -68,29 +74,39 @@ def set_outliers_to_null(df):
             
     return(df)
 
+def impute_mean(df, na_cols):
+    for i in na_cols:
+        if i in num_cols:
+            df.loc[df[i].isnull(), i] = df[i].dropna().mean()
+        elif i in str_cols:
+            df.loc[df[i].isnull(), i] = df[i].dropna().mode()            
+    return(df)
+
+
 def handle_nas(df_impute, method='impute_mean'):
     
     df = set_outliers_to_null(df_impute)
 
     df = df.drop(columns=list(df.isnull().sum()[np.sum(df.isnull())>0.4*df.shape[0]].index))
 
-    na_col = list(df.isnull().sum().sort_values(ascending=True)[np.sum(df.isnull())>0].index)
+    na_cols = list(df.isnull().sum().sort_values(ascending=True)[np.sum(df.isnull())>0].index)
     num_cols = list(df.columns[(df.dtypes == 'float64') | (df.dtypes == 'int64')])
     str_cols = list(set(df.columns)^set(num_cols))
+    predictors = list(set(na_cols)^set(df.columns))
     
-    print('Imputing data for variables: %s'%na_col)
+    print('Imputing data for variables: %s \n Total number of features for imputing: %s \n Total Columns: %s'
+          %(na_cols, len(na_cols), len(list(df.columns))))
+    
+    # if all cols are na's impute, one with least na's by mean
+    if len(predictors) == 0:
+        df = impute_mean(df=df, na_cols=na_cols[0])
+        na_cols = list(df.isnull().sum().sort_values(ascending=True)[np.sum(df.isnull())>0].index)
+        predictors = list(set(na_cols)^set(df.columns))
 
-    if method == 'drop':
-        df=df.dropna()
-    elif method == 'impute_mean':
-        for i in list(df[na_col].columns):
-            if i in num_cols:
-                df.loc[df[i].isnull(), i] = df[i].dropna().mean()
-            elif i in str_cols:
-                df.loc[df[i].isnull(), i] = df[i].dropna().mode()
-    elif method == 'impute_predict':
-        predictors = list(set(na_col)^set(df.columns))
-        for i in na_col:
+    if method == 'impute_mean':
+        df = impute_mean(df=df, na_cols=na_cols)
+    elif method == 'impute_predict':        
+        for i in na_cols:
             print('loop started for %s...'%i)
             X = df[predictors]
             X_scaled = scale_encode(X, X)
@@ -99,16 +115,12 @@ def handle_nas(df_impute, method='impute_mean'):
             X_pred = X_scaled.loc[df[i].isnull()]
             print('   scaling done...')
             if i in num_cols:
-                knn = KNeighborsRegressor(15).fit(X_train, y)
+                knn = KNeighborsRegressor(30).fit(X_train, y)
                 print('   fit model...')
-                display(df.loc[df[i].isnull(), i].shape)
-                display(knn.predict(X_pred).shape)
                 df.loc[df[i].isnull(), i] = knn.predict(X_pred)
             elif i in str_cols:
-                knn = KNeighborsClassifier(15).fit(X_train, y)
+                knn = KNeighborsClassifier(30).fit(X_train, y)
                 print('   fit model...')
-                display(df.loc[df[i].isnull(), i].shape)
-                display(knn.predict(X_pred).shape)
                 df.loc[df[i].isnull(), i] = knn.predict(X_pred)
                 predictors.append(i)
             print('   %s done...'%i)
